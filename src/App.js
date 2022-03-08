b2014c62eeec44090808ea52df11e2fd71f0a1bb
Goutam Nath <nathgoutam93@gmail.com>
2022-03-08 17:06:10 +0530

    A Basic Working Collaborative Rich Text Editor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+
+const TOOLBAR_OPTIONS = [
+  [{}],
+  ["bold", "italic"],
+  ["blockquote", "code-block", "link"],
+  [{ list: "bullet" }, { list: "ordered" }],
+  ["image"],
+];
 
 export default function App() {
-  return <div>Collaborative Editor</div>;
+  const [quill, setQuill] = useState(null);
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+
+    socket.on("receive-changes", handler);
+
+    return () => {
+      quill.off("receive-changes");
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+
+    quill.on("text-change", handler);
+
+    return () => {
+      quill.off("text-change");
+    };
+  }, [socket, quill]);
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    setQuill(q);
+  }, []);
+
+  return <div className="container" ref={wrapperRef}></div>;
 }
