4352466e8414242da2b3de171fa57e6fc44e5bbe
Goutam Nath <nathgoutam93@gmail.com>
2022-03-31 00:45:05 +0530

    Hot fix 🚨 toast bg-color

--- src/components/commons/toast.js
+++ src/components/commons/toast.js
@@ -7,25 +7,25 @@ import {
 import { BiError } from "react-icons/bi";
 
 export default function Toast({ show, type, message }) {
-  const [color, setColor] = useState("green");
+  const [color, setColor] = useState("#16de69");
   const [icon, setIcon] = useState(<HiOutlineInformationCircle size={35} />);
 
   useEffect(() => {
     switch (type) {
       case "info":
-        setColor("blue");
+        setColor("#457aff");
         setIcon(<HiOutlineInformationCircle size={35} />);
         break;
       case "error":
-        setColor("red");
+        setColor("#c40000");
         setIcon(<BiError size={35} />);
         break;
       case "success":
-        setColor("green");
+        setColor("#16de69");
         setIcon(<HiOutlineBadgeCheck size={35} />);
         break;
       default:
-        setColor("green");
+        setColor("#16de69");
         setIcon(<HiOutlineBadgeCheck size={35} />);
         break;
     }
@@ -33,9 +33,10 @@ export default function Toast({ show, type, message }) {
 
   return (
     <div
+      style={{ backgroundColor: color }}
       className={`${
         show ? "opacity-100 right-4 lg:right-20" : "opacity-0 -right-10"
-      } fixed bottom-3/4 lg:bottom-20 p-4 flex justify-center items-center font-nunito text-white bg-${color}-500 rounded-xl space-x-2 z-50 transition-all pointer-events-none`}
+      } fixed bottom-3/4 lg:bottom-20 p-4 flex justify-center items-center font-nunito text-white rounded-xl space-x-2 z-50 transition-all pointer-events-none`}
     >
       {icon}
       {message}
